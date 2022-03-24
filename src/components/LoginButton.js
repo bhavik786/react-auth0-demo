a5632be28a19e0177f3f2e8ed716adca4ef0f255
Bhavik Gevariya <bgevariya20@gmail.com>
2022-03-24 23:55:32 +0530

    simple auth0

---
+++ src/components/LoginButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "reactstrap";
+const LoginButton = () => {
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  return (
+    !isAuthenticated && (
+      <Button
+        style={{ marginTop: "25%" }}
+        color="primary"
+        onClick={() => loginWithRedirect()}
+      >
+        Log In
+      </Button>
+    )
+  );
+};
+
+export default LoginButton;
